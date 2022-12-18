3dfc0b9d991951cc8c8fd9af8e0757d6b3883ad3
50^2 <alexeusgr@gmail.com>
2022-12-18 16:18:45 +0200

    list of strings with delimeters for sub-lists

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ main :: IO()
 main = do  
         contents <- readFile "data-1"
 
-        print (splitOn "" (words contents))
+        print . lines $ contents
 -- alternately, main = print . map readInt . words =<< readFile "test.txt"
 
 readInt :: String -> Int
