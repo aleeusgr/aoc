5b1f5b1c03f11eb2aeee8fadf2e48b50f16c4f9c
50^2 <alexeusgr@gmail.com>
2022-12-21 09:19:15 +0200

    got [[Int],[]]

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,9 @@ main = do
         contents <- readFile "data-1"
 
         print . splitOn [""] . lines $ contents
+        -- map (read::String->Int) ((splitOn [""] . lines $ contents) !! 1)
+        -- map (map (read::String->Int)) (splitOn [""] . lines $ contents)
+
 -- alternately, main = print . map readInt . words =<< readFile "test.txt"
 
 readInt :: String -> Int
