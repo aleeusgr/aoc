be161832243d852d88e0919c7fc2d1bd5c3dbb1c
50^2 <alexeusgr@gmail.com>
2022-12-21 10:27:00 +0200

    no1 done

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,15 @@
 module Main where
 
 import Data.List.Split
+import Data.List
+
+--hay = map sum (map (map (read::String->Int)) (splitOn [""] . lines $ contents)) 
 
 main :: IO()
 main = do  
         contents <- readFile "data-1"
 
-        print . splitOn [""] . lines $ contents
-        -- map (read::String->Int) ((splitOn [""] . lines $ contents) !! 1)
-        -- map (map (read::String->Int)) (splitOn [""] . lines $ contents)
-
--- alternately, main = print . map readInt . words =<< readFile "test.txt"
-
-readInt :: String -> Int
-readInt = read
+        let hay = map (map (read::String->Int)) (splitOn [""] . lines $ contents)
+        let needle =maximum $ map sum hay
+        print needle
 
