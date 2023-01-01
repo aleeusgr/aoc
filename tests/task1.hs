7a62da2c594ece8f4b4e30a730189f8cf92cb19e
50^2 <alexeusgr@gmail.com>
2023-01-01 14:49:29 +0200

    how to build a test?

---
+++ tests/task1.hs
@@ -0,0 +1,5 @@
+-- Ok, this is just same program in haskell, bt its designed to compare the output user provides to a verified answer.
+--
+-- In simplest case, I have a file and an answer I got verification from authors. Therefore I am sure my solution is correct. 
+-- Now the case is to do this in a more authentic way
+-- How to add a test to a Haskell app? This is more of a cabal question.
