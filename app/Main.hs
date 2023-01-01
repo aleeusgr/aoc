25646bba140a2a66eeaceebbc0b31fd78a45db65
50^2 <alexeusgr@gmail.com>
2023-01-01 14:39:43 +0200

    fix IO

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,15 @@ module Main where
 
 import Data.List.Split
 
-task1 :: IO String -> IO()
+task1 :: String -> Int
 task1 contents = 
-
   let  
       hay = map (map (read::String->Int)) (splitOn [""] . lines $ contents)
       needle = maximum $ map sum hay
 
-  in print needle
+  in needle
 
 main :: IO()
 main = do  
-      task1 $ readFile "data-1"
+      dt <- readFile "data-1"
+      print $ task1 dt
