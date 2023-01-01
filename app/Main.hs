b56aed85a53104d74c09d6b01f1cfac31d4eac8b
50^2 <alexeusgr@gmail.com>
2023-01-01 10:31:44 +0200

    IO error

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,15 @@ module Main where
 
 import Data.List.Split
 
+task1 :: IO String -> IO()
+task1 contents = 
+
+  let  
+      hay = map (map (read::String->Int)) (splitOn [""] . lines $ contents)
+      needle = maximum $ map sum hay
+
+  in print needle
+
 main :: IO()
 main = do  
-        contents <- readFile "data-1"
-
-        let hay = map (map (read::String->Int)) (splitOn [""] . lines $ contents)
-        let needle =maximum $ map sum hay
-        print needle
-
+      task1 $ readFile "data-1"
