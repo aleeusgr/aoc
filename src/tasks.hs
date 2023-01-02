5acccb3f0637293eff50d4f6bc5baaa431c3b1a4
50^2 <alexeusgr@gmail.com>
2023-01-02 09:16:39 +0200

    missed

---
+++ src/tasks.hs
@@ -0,0 +1,11 @@
+module tasks where
+
+import Data.List.Split
+
+task1 :: String -> Int
+task1 contents = 
+  let  
+      hay = map (map (read::String->Int)) (splitOn [""] . lines $ contents)
+      needle = maximum $ map sum hay
+
+  in needle
