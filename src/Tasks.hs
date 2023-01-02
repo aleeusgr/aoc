0538b86c7c720671f8513663b8b339e85d9b1a56
50^2 <alexeusgr@gmail.com>
2023-01-02 09:48:58 +0200

    user test OK, cabal test OK, target test is empty

---
+++ src/Tasks.hs
@@ -0,0 +1,11 @@
+module Tasks where
+
+import Data.List.Split
+
+task1 :: String -> Int
+task1 contents = 
+  let  
+      hay = map (map (read::String->Int)) (splitOn [""] . lines $ contents)
+      needle = maximum $ map sum hay
+
+  in needle
